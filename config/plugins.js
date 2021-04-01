14f0a3d70b44e1380c3beedd5085ba2e9957c3a2
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-04-01 20:05:14 +0100

    updated email plugin

--- config/plugins.js
+++ config/plugins.js
@@ -13,6 +13,17 @@ module.exports = ({ env }) => {
           },
         },
       },
+      email: {
+        provider: "sendgrid",
+        providerOptions: {
+          apiKey: env("SENDGRID_API_KEY"),
+        },
+        settings: {
+          defaultFrom: "sam@execbjj.com",
+          defaultReplyTo: "sam@execbjj.com",
+          testAddress: "sam.mcnally94@gmail.com",
+        },
+      },
       // ...
     };
   }
