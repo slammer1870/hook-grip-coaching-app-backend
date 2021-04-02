245270380e68281fe07e062e1977bd788d1dc2c4
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-04-02 13:42:46 +0100

    revoked curriculum change

--- api/curriculum/controllers/curriculum.js
+++ api/curriculum/controllers/curriculum.js
@@ -3,42 +3,6 @@ const { sanitizeEntity } = require("strapi-utils");
 const finder = require("strapi-utils/lib/finder");
 
 module.exports = {
-  /**
-   * Only returns orders that belong to the logged in user
-   * @param {any} ctx
-   */
-  async find(ctx) {
-    const { user } = ctx.state; //Magic user
-
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.order.search({
-        ...ctx.query,
-        user: user.id,
-      });
-    } else {
-      entities = await strapi.services.order.find({
-        ...ctx.query,
-        user: user.id,
-      });
-    }
-
-    return entities.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models.order })
-    );
-  },
-  /**
-   * Returns one order, as long as it belongs to the user
-   * @param {any} ctx
-   */
-
-  async findOne(ctx) {
-    const { id } = ctx.params;
-    const { user } = ctx.state;
-
-    const entity = await strapi.services.order.findOne({ id, user: user.id });
-    return sanitizeEntity(entity, { model: strapi.models.order });
-  },
 
   /**
    * Returns one order, as long as it belongs to the user
