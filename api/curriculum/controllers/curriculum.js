dd6a07336eb514040361ca352d47c92b35e25639
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-04-10 19:46:06 +0100

    course and curriculum responses limited

--- api/curriculum/controllers/curriculum.js
+++ api/curriculum/controllers/curriculum.js
@@ -3,25 +3,29 @@ const { sanitizeEntity } = require("strapi-utils");
 const finder = require("strapi-utils/lib/finder");
 
 module.exports = {
+  /**
+   * Returns one order, as long as it belongs to the user
+   * @param {any} ctx
+   */
 
-  /*async find(ctx) {
+  async find(ctx) {
     const { user } = ctx.state; //Magic user
 
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.order.search({
+      entities = await strapi.services.curriculum.search({
         ...ctx.query,
         user: user.id,
       });
     } else {
-      entities = await strapi.services.order.find({
+      entities = await strapi.services.curriculum.find({
         ...ctx.query,
         user: user.id,
       });
     }
 
     return entities.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models.order })
+      sanitizeEntity(entity, { model: strapi.models.curriculum })
     );
   },
   /**
@@ -29,17 +33,14 @@ module.exports = {
    * @param {any} ctx
    */
 
-  /*async findOne(ctx) {
+  async findOne(ctx) {
+    console.log(ctx.params)
     const { id } = ctx.params;
     const { user } = ctx.state;
 
-    const entity = await strapi.services.order.findOne({ id, user: user.id });
-    return sanitizeEntity(entity, { model: strapi.models.order });
+    const entity = await strapi.services.curriculum.findOne({ id: id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.curriculum });
   },
-  /**
-   * Returns one order, as long as it belongs to the user
-   * @param {any} ctx
-   */
 
   async create(ctx) {
     const { timeslot, name } = ctx.request.body;
@@ -50,9 +51,9 @@ module.exports = {
     const newCurriculum = await strapi.services.curriculum.create({
       user: user.id,
       timeslot: timeslot.id,
-      name: name
+      name: name,
     });
 
-    return { id: newCurriculum.id };
-  }
+    return newCurriculum ;
+  },
 };
